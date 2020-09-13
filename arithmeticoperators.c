9f66aa41dfe6137beca23e238d61462c720dd1fa
Jasjot Singh <jasjot784@gmail.com>
2020-09-13 20:01:03 +0530

    Arithmetic operators

---
+++ arithmeticoperators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+  int a,b;
+  printf("Enter the first number\n");
+  scanf("%d\n",&a);
+  printf("Enter the second number\n");
+  scanf("%d\n",&b);
+
+  printf("a / b = %d\n",a/b);
+  printf("a %% b = %d\n",a%b);
+}
