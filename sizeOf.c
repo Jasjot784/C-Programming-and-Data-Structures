0075092c84c194d4b0c8c2015196b49e95ef99d3
Jasjot Singh <jasjot784@gmail.com>
2020-09-13 10:15:30 +0530

    size of int and modifires

---
+++ sizeOf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  printf("%d",sizeof(int));
+  printf("%d",sizeof(short int));
+  printf("%d",sizeof(short short int));
+  printf("%d",sizeof(long int));
+  printf("%d",sizeof(long long int));
+}
