c793f02832b0456d298daeb14df73b9d88446a0a
Jasjot Singh <jasjot784@gmail.com>
2020-09-15 09:18:37 +0530

    Indirect recursion

---
+++ recursionindirect.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void odd();
+void even();
+int n = 1;
+
+int main(){
+  odd();
+}
+
+void odd(){
+  if (n<=10) {
+    /* code */
+    printf("%d ",n+1);
+    n++;
+    even();
+  }return;
+}
+
+void even(){
+  if(n<=10){
+    printf("%d ",n-1);
+    n++;
+    odd();
+  }
+  return;
+}
